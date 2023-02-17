e4ff3b8c17357a804d55309769d83c797d997de1
Darrel O'Pry <darrel.opry@spry-group.com>
2023-02-17 05:02:16 -0500

    feat: post meter association

--- src/types/api/meter.ts
+++ src/types/api/meter.ts
@@ -10,15 +10,18 @@ export interface IMeterMeterGetResponse extends IParsedXml {
     meter: IMeter;
 }
 
-export interface IMeterMeterPostResponse extends IParsedXml { 
+export interface IMeterMeterPostResponse extends IParsedXml {
     response: IResponse
 }
 
-export interface IMeterMeterListGetResponse extends IParsedXml { 
+export interface IMeterMeterListGetResponse extends IParsedXml {
     response: IResponse
 }
 
 export interface IMeterPropertyAssociationGetResponse extends IParsedXml {
     meterPropertyAssociationList: IMeterPropertyAssociationList;
   }
-  
+
+export interface IMeterPropertyAssociationPostResponse extends IParsedXml {
+    response: IResponse;
+}
