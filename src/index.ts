fdba845f310c9e07e744d539b1264b930d112d9c
Darrel O'Pry <darrel.opry@spry-group.com>
2023-11-18 13:05:12 -0500

    feat: build and publish esm only

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
+import * as url from "node:url";
 
+export * from "./PortfolioManager";
+export * from "./PortfolioManagerApi";
 export * from "./cli";
 export * from "./types";
-export * from "./PortfolioManagerApi";
-export * from "./PortfolioManager";
 
 import { PortfolioManagerCommand } from "./cli/PortfolioManagerCommand";
 
@@ -12,6 +13,9 @@ async function main() {
   cli.parse(process.argv);
 }
 
-if (require.main === module) {
-  main();
+if (import.meta.url.startsWith("file:")) {
+  const modulePath = url.fileURLToPath(import.meta.url);
+  if (process.argv[1] === modulePath) {
+    main();
+  }
 }
